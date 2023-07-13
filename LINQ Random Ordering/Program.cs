d85805e4bfda0f04f904bbf693400f4e72f37a66
Kulpot <kulpotwahu@gmail.com>
2023-07-13 20:43:27 +0800

    Add project files.

---
+++ LINQ Random Ordering/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=thZMASt-EUY&list=PL90AF0EFFEF782D27&index=19
+// Random Ordering --- rand.Next()
+
+class MainClass
+{
+    static void Main()
+    {
+        var rand = new Random();
+        var contactNames =
+           //DB.Customers.OrderBy(c => c.ContactName).Select(c => c.ContactName);
+           DB.Customers.OrderBy(c => rand.Next()).Select(c => c.ContactName).Take(5);
+        foreach (string name in contactNames)
+            Console.WriteLine(name);
+    }
+}
